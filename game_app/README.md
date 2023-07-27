d9d4837b2513a03bb523e96335d162a07adfa10a
Stefan <epiktetov.sv@gmail.com>
2023-07-27 20:51:28 +0300

    add onion_arch

---
+++ game_app/README.md
@@ -0,0 +1,47 @@
+## 👾 RSHB Game API
+![FastAPI](https://img.shields.io/badge/FastAPI-blue)
+![Gunicorn](https://img.shields.io/badge/Gunicorn-green)
+![PostgreSQL](https://img.shields.io/badge/PostgreSQL-blue)
+![pgAdmin4](https://img.shields.io/badge/pgAdmin4-blue)
+![Nginx](https://img.shields.io/badge/Nginx-green)
+![Docker](https://img.shields.io/badge/Docker-blue)
+
+АPI for RSHB Bank's browser game
+
+<img src="https://github.com/StefanEpic/fastapi_rshb_hakaton/blob/main/game_app/media/001.jpg" width="600">
+
+## 🚀 Usage
+The API returns information for a browser game, implemented on the frontend
+
+### API method results. Example values | Schema:
+Returns a list of game locations with number and name:
+`GET /`
+```
+[
+  {
+    "number": 0,
+    "title": "string",
+    "about": "string"
+  }
+]
+```
+Returns a list of location dialogues, taking into account the gender of the player:
+`GET /dialog/?location_number=<int>&gender=<str>`
+```
+[
+  {
+    "number": 0,
+    "text": "string"
+  }
+]
+```
+Returns a list of location images:
+`GET /image/?location_number=<int>`
+```
+[
+  {
+    "title": "string",
+    "data": "string"
+  }
+]
+```
--- game_app/game/models.py
+++ game_app/game/models.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
 
+from .schemas import SchemaLocation, SchemaDialog, SchemaImage
+
 
 class Location(Base):
     __tablename__ = "location"
@@ -14,6 +16,13 @@ class Location(Base):
     number: Mapped[int] = mapped_column(unique=True)
     title: Mapped[str] = mapped_column(unique=True)
     about: Mapped[Optional[str]]
+
+    def to_read_model(self) -> SchemaLocation:
+        return SchemaLocation(
+            number=self.number,
+            title=self.title,
+            about=self.about if self.about else None
+        )
 
 
 class Dialog(Base):
@@ -25,6 +34,12 @@ class Dialog(Base):
     gender: Mapped[str] = mapped_column(String(1))
     text: Mapped[Optional[str]]
 
+    def to_read_model(self) -> SchemaDialog:
+        return SchemaDialog(
+            number=self.number,
+            text=self.text
+        )
+
 
 class Image(Base):
     __tablename__ = "image"
@@ -33,3 +48,9 @@ class Image(Base):
     location = mapped_column(ForeignKey("location.number"))
     title: Mapped[str] = mapped_column(unique=True)
     data: Mapped[str]
+
+    def to_read_model(self) -> SchemaImage:
+        return SchemaImage(
+            title=self.title,
+            data=self.data
+        )
