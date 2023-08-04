040e7e76b9585219b00726612e9bb7fea0d74e7b
Stefan <epiktetov.sv@gmail.com>
2023-08-04 19:35:31 +0300

    fix nginx

--- src/db/db.py
+++ src/db/db.py
@@ -12,12 +12,15 @@ DB_HOST = os.environ.get('DB_HOST')
 DB_NAME = os.environ.get('DB_NAME')
 DB_PORT = os.environ.get('DB_PORT')
 
+SITE = os.environ.get('SITE')
+
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 MEDIA_URL = f'{os.path.abspath(os.curdir)}/media'
-SITE = 'http://127.0.0.1:8080/media/'
+SITE = f'{SITE}/media/'
+
 
 class Base(DeclarativeBase):
     pass
